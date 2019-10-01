78da068a866f936a63f2e607a94c2d3a4bbf4c82
Shubham Kotwal <shubhamkotwal1225@gmail.com>
2019-10-01 20:32:49 +0530

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,1 +1,13 @@
 
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  database="mysql"
+)
+mycursor = mydb.cursor()
+a = input("Enter Password:")
+flush = "FLUSH PRIVILEGES"
+mycursor.execute("UPDATE user SET password=PASSWORD('%s') WHERE User='root' AND Host = 'localhost'" % a)
+mycursor.execute(flush)
+mydb.commit()
