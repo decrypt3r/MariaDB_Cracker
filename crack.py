a32aa47a61582e58953256d5d6a5d3fcc34b9e5e
Shubham Kotwal <shubhamkotwal1225@gmail.com>
2019-10-01 20:37:34 +0530

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,5 +1,13 @@
-
 import mysql.connector
+import os
+
+#####skip-grants-table#####
+systemctl stop mariadb
+systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
+systemctl start mariadb
+###########################
+
+#####Main-Code#####
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -11,3 +19,10 @@ flush = "FLUSH PRIVILEGES"
 mycursor.execute("UPDATE user SET password=PASSWORD('%s') WHERE User='root' AND Host = 'localhost'" % a)
 mycursor.execute(flush)
 mydb.commit()
+####################
+
+#####Unset-skipping#####
+systemctl stop mariadb
+systemctl unset-environment MYSQLD_OPTS
+systemctl start mariadb
+########################
