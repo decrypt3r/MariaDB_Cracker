7e365d77804067c5c3a9b4e314420ea69ab6308f
Shubham Kotwal <shubhamkotwal1225@gmail.com>
2019-10-05 16:34:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,25 +14,45 @@ echo
 echo
 echo -e ${RED} "Installation Started..."
 
-
-
-cat /etc/*-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | tr -d \" > os.txt
+cat /etc/*-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | tr -d \" | awk '{print $1;}' > os.txt
 os=`cat os.txt`
 
+#####For Redhat & CentOS#####
+if [ "$os" == "Red" ] || [ "$os" == "CentOS" ]
+then
+        find /bin/ -name "figlet" > figlet.txt
+        fig=`cat figlet.txt`
+        if [[ $fig == *"/bin/figlet"* ]]; then
+                echo "Figlet is Installed"
+        else
+                yum install wget
+                wget http://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/f/figlet-2.2.5-9.el7.x86_64.rpm
+                rpm -i figlet-2.2.5-9.el7.x86_64.rpm
+        fi
+##############################
 
-yum install wget
-wget http://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/f/figlet-2.2.5-9.el7.x86_64.rpm
-rpm -i figlet-2.2.5-9.el7.x86_64.rpm
+
+#####Other Linux#####
+else
+        find /bin/ -name "figlet" > figlet.txt
+        fig=`cat figlet.txt`
+        if [[ $fig == *"/bin/figlet"* ]]; then
+                echo "Figlet is Installed"
+        else
+                apt-get install figlet
+        fi
+#####################
+fi
+
 
 ################################
 
 
 #####Move to bin and give permission#####
-mv marcrack /usr/bin
-mkdir -p /usr/bin/marcrack
-mv crack.py /usr/bin/marcrack
+cp marcrack /usr/bin/
+mkdir -p /usr/bin/marcrackk
+cp crack.py /usr/bin/marcrackk/
 chmod a+x /usr/bin/marcrack
-chmod a+x /usr/bin/marcrack/crack.py
+chmod a+x /usr/bin/marcrackk/crack.py
 #########################################
-
 
